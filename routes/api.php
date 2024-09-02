bf74ce38937e29ce7bee71cdd27ae88a1ba3c115
adalbertog20 <adalbertog_20@alu.uabcs.mx>
2024-09-01 19:29:07 -0700

    quitar middleware de rutas de la api

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,15 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 
-Route::middleware(['auth:sanctum'])->group(function () {
+//Route::middleware(['auth:sanctum'])->group(function () {
+//    Route::put("/update-profile", [UpdatedUserController::class, 'update']);
+//    Route::put('/update-password', [UpdatedUserController::class, 'updatePassword']);
+//    Route::put('/update-jobs', [UpdatedUserController::class, 'updateJobs']);
+//    Route::get('/show-jobs', [UserController::class, 'showJobs']);
+//    Route::put('/update-role', [UserController::class, 'updateRole']);
+//});
+
+Route::controller(UpdatedUserController::class)->group(function () {
     Route::put("/update-profile", [UpdatedUserController::class, 'update']);
     Route::put('/update-password', [UpdatedUserController::class, 'updatePassword']);
     Route::put('/update-jobs', [UpdatedUserController::class, 'updateJobs']);
@@ -22,7 +30,15 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::put('/update-role', [UserController::class, 'updateRole']);
 });
 
-Route::middleware(['auth:sanctum'])->group(function () {
+//Route::middleware(['auth:sanctum'])->group(function () {
+//    Route::post('/chamba', [ChambaController::class, 'store'])->middleware(RedirectClient::class)->name('chamba.store');
+//    Route::get('/chamba/{id}', [ChambaController::class, 'show'])->name('chamba.show');
+//    Route::delete('/chamba/{id}', [ChambaController::class, 'destroy'])->middleware(RedirectClient::class)->name('chamba.destroy');
+//    Route::put('/chamba/{id}', [ChambaController::class, 'update'])->middleware(RedirectClient::class)->name('chamba.update');
+//    Route::get('/chambas', [ChambaController::class, 'index'])->name('chamba.index');
+//});
+
+Route::controller(ChambaController::class)->group(function () {
     Route::post('/chamba', [ChambaController::class, 'store'])->middleware(RedirectClient::class)->name('chamba.store');
     Route::get('/chamba/{id}', [ChambaController::class, 'show'])->name('chamba.show');
     Route::delete('/chamba/{id}', [ChambaController::class, 'destroy'])->middleware(RedirectClient::class)->name('chamba.destroy');
@@ -30,7 +46,13 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/chambas', [ChambaController::class, 'index'])->name('chamba.index');
 });
 
-Route::middleware(['auth:sanctum', 'can:isAdmin'])->group(function () {
+//Route::middleware(['auth:sanctum', 'can:isAdmin'])->group(function () {
+//    Route::get('/requests', [RequestsChambasController::class, 'getAllRequests'])->name('requests.index');
+//    Route::post('/requests', [RequestsChambasController::class, 'store'])->name('requests.store');
+//    Route::put('/requests-status/{id}', [RequestsChambasController::class, 'updateStatus'])->name('status.update');
+//});
+
+Route::controller(RequestsChambasController::class)->group(function () {
     Route::get('/requests', [RequestsChambasController::class, 'getAllRequests'])->name('requests.index');
     Route::post('/requests', [RequestsChambasController::class, 'store'])->name('requests.store');
     Route::put('/requests-status/{id}', [RequestsChambasController::class, 'updateStatus'])->name('status.update');
